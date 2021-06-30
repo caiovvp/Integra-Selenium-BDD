b8fcb7c84cdcba9e34c8d632ed16df4e9517eee5
Caio Pedreira <cacadummestre@gmail.com>
2021-06-30 17:55:23 -0400

    view_executions feature teste

--- features/feature/view_executions.feature
+++ features/feature/view_executions.feature
@@ -1,15 +1,17 @@
     Feature: View executions of all processes
 
-      Background: Log in Integra and go to Executions
+      Scenario: Log in Integra and go to Executions
         Given user is logged in Integra
         And go to the Executions tab
 
-        Scenario: View all the integrations
-          When go to all processes
-          Then show the all the executions of all processes
-
-
-        Scenario: View executions of each process
-          When open list of processes
-          And select a process of the list
-          Then show a list of all executions on that process
+      Scenario: View all the integrations
+        When go to all processes
+        Then show the all the executions of all processes
+        Scenario: Test if filter is working
+          When choose a filter
+          Then show only processes according with that filter
+#
+#      Scenario: View executions of each process
+#        When select a process of the list
+#        Then show a list of all executions on that process
+#
--- features/steps/view_executions_steps.py
+++ features/steps/view_executions_steps.py
@@ -1,5 +1,3 @@
-import time
-
 from behave import *
 
 from features.contexts.view_executions_ctx import EXECUTIONS_TAB, ALL_PROCESSES, PROCESS_TITLE, OPEN_LIST_BTN, \
@@ -22,13 +20,32 @@ def step_impl(context):
     assert process_title.text == 'Todos as execuções do processo'
 
 
-@when('open list of processes')
+@when('choose a filter')
 def step_impl(context):
-    context.browser.find_element_by_xpath(OPEN_LIST_BTN).click()
+    select_filter = context.browser.find_element_by_id('filter-by-status')
+    select_options = select_filter.find_elements_by_tag_name('option')
+    max_length = 20
+    for i in select_options:
+        select_filter.click()
+        i.click()
+        executions_div = context.browser.find_element_by_xpath('/html/body/main/div[2]/div[1]/div[2]/div')
+        executions_empty = executions_div.find_elements_by_class_name('d-none')
+        executions_length = 10 - len(executions_empty)
+        max_length -= executions_length
+    assert max_length == 0
+
+
+@then('show only processes according with that filter')
+def step_impl(context):
+    pass
 
 
 @when('select a process of the list')
-def get_li(context):
+def open_list(context):
+    context.browser.find_element_by_xpath(OPEN_LIST_BTN).click()
+
+
+def get_executions(context):
     list_itens = context.browser.find_element_by_xpath(UL_OPTIONS)
     executions_li = list_itens.find_elements_by_tag_name('li')
     return executions_li
@@ -36,14 +53,14 @@ def get_li(context):
 
 @then('show a list of all executions on that process')
 def step_impl(context):
-    executions_li = get_li(context)
-    list_length = len(executions_li)
-    for i in range(list_length):
+    i = 0
+    for li in get_executions(context):
         if i >= 1:
-            context.execute_steps(u'''when open list of processes''')
-            executions_li = get_li(context)
-        execution_name = executions_li[i].find_element_by_tag_name('span').text
-        executions_li[i].click()
+            open_list(context)
+            li = (get_executions(context))[i]
+        execution_name = li.find_element_by_class_name('text').text
+        li.click()
         context.browser.find_element_by_xpath(SHOW_EXECUTIONS_BTN).click()
         execution_title = context.browser.find_element_by_xpath(EXECUTION_TITLE).text
         assert execution_name in execution_title
+        i += 1
